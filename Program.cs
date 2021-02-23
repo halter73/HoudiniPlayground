77c5468449f148477a03712d7ff74c58de50742c
Stephen Halter <halter73@gmail.com>
2021-02-22 18:42:28 -0800

    Add example with ambiguous return type

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ var app = WebApplication.Create(args);
 app.MapAction([HttpPost("/")] ([FromBody] Todo todo) : Todo => todo);
 app.MapAction([HttpGet("/")] () : Todo => new(Id: 0, Name: "Play more!", IsComplete: false));
 
+app.MapAction([HttpGet("/id/{id?}")] ([FromRoute] int? id) : IResult => 
+    id is null ?
+    new StatusCodePagesExtensions(404) :
+    new JsonResult(new Todo(Id: id.Value, Name: "From id!", IsComplete: false)));
+
 await app.RunAsync();
 
 record Todo(int Id, string Name, bool IsComplete);
