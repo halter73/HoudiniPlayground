452dba5f72aebac080fb4d942c0751318d6f6162
Stephen Halter <halter73@gmail.com>
2021-02-22 16:14:34 -0800

    Use MapAction!

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
-using System.Threading.Tasks;
+using System;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 var app = WebApplication.Create(args);
 
-app.MapGet("/", async http =>
-{
-    await http.Response.WriteAsync("Hello World!");
-});
+[HttpGet("/")]
+Todo GetTodo() => new(Id: 0, Name: "Play more!", IsComplete: false);
+app.MapAction((Func<Todo>)GetTodo);
+
+[HttpPost("/")]
+Todo EchoTodo([FromBody] Todo todo) => todo;
+app.MapAction((Func<Todo, Todo>)EchoTodo);
 
 await app.RunAsync();
+
+record Todo(int Id, string Name, bool IsComplete);
