574242d9b971620b4e7e7e71c69240be1a053667
Stephen Halter <halter73@gmail.com>
2021-02-22 18:33:48 -0800

    Add GetTodoFromId

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 var app = WebApplication.Create(args);
@@ -11,6 +12,13 @@ app.MapAction((Func<Todo>)GetTodo);
 [HttpPost("/")]
 Todo EchoTodo([FromBody] Todo todo) => todo;
 app.MapAction((Func<Todo, Todo>)EchoTodo);
+
+[HttpGet("/id/{id?}")]
+IResult GetTodoFromId([FromRoute] int? id) =>
+    id is null ?
+    new StatusCodeResult(404) :
+    new JsonResult(new Todo(Id: id.Value, Name: "From id!", IsComplete: false));
+app.MapAction((Func<int?, IResult>)GetTodoFromId);
 
 await app.RunAsync();
 
