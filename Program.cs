f75f2d09658fc93debce2c8d47a2dc1264855e14
Stephen Halter <halter73@gmail.com>
2021-02-22 16:29:47 -0800

    MapAction with lamdas!

--- Program.cs
+++ Program.cs
@@ -4,13 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 var app = WebApplication.Create(args);
 
-[HttpGet("/")]
-Todo GetTodo() => new(Id: 0, Name: "Play more!", IsComplete: false);
-app.MapAction((Func<Todo>)GetTodo);
-
-[HttpPost("/")]
-Todo EchoTodo([FromBody] Todo todo) => todo;
-app.MapAction((Func<Todo, Todo>)EchoTodo);
+app.MapAction([HttpGet("/")] () => new(Id: 0, Name: "Play more!", IsComplete: false));
+app.MapAction([HttpPost("/")] ([FromBody] Todo todo) => todo);
 
 await app.RunAsync();
 
